4e360e7905955814bafe6e83799e11d181c56860
Sara Chein <sarahchein@gmail.com>
2024-11-25 13:47:30 -0500

    user

--- api/server.js
+++ api/server.js
@@ -1,10 +1,34 @@
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
 
 server.get('/api/users', (req, res) => {
-    console.log('getting all users')
-    res.json('users')
+    User.find()
+    .then(users => {
+        console.log(users)
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+       console.log(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 })
 
 server.use('*', (req, res) => {
